c9679a6dcc1e4f34fd18426e7c60dc1222b64ca0
ejungwoo <phyjics@gmail.com>
2019-05-04 05:22:28 +0900

    mc macro simplified

--- macros/lamps.mc.cc
+++ macros/lamps.mc.cc
@@ -2,9 +2,6 @@
 #include "KBCompiled.h"
 
 #include "KBG4RunManager.hh"
-#include "G4VisExecutive.hh"
-#include "G4UImanager.hh"
-#include "G4UIExecutive.hh"
 #include "G4StepLimiterPhysics.hh"
 
 #include "KBParameterContainer.hh"
@@ -26,34 +23,14 @@ int main(int argc, char** argv)
   physicsList -> RegisterPhysics(new G4StepLimiterPhysics());
 
   runManager -> SetUserInitialization(physicsList);
-  if (argc >= 2)
-    runManager -> SetParameterContainer(argv[2]);
-  else
-    runManager -> SetParameterContainer("tpc_rec.par");
+  runManager -> SetParameterContainer(argv[1]);
   runManager -> SetUserInitialization(new LHDetectorConstruction());
   runManager -> SetUserAction(new KBPrimaryGeneratorAction());
   runManager -> SetUserAction(new KBEventAction());
   runManager -> SetUserAction(new KBTrackingAction());
   runManager -> SetUserAction(new KBSteppingAction());
   runManager -> Initialize();
-
-  G4UImanager* uiManager = G4UImanager::GetUIpointer();
-  if (argc != 1) {
-    G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    uiManager -> ApplyCommand(command+fileName);
-  }
-  else {
-    G4VisManager* visManager = new G4VisExecutive;
-    visManager -> Initialize();
-
-    G4UIExecutive* uiExecutive = new G4UIExecutive(argc, argv);
-    uiManager -> ApplyCommand("/control/execute vis.mac"); 
-    uiExecutive -> SessionStart();
-
-    delete uiExecutive;
-    delete visManager;
-  }
+  runManager -> Run();
 
   delete runManager;
 
